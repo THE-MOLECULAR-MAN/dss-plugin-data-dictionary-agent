c0c2629e981b602792c380af3e2eb4c88753bd0e
demo <tim.honker@dataiku.com>
2025-10-30 20:21:05 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -199,5 +199,6 @@ class MyRunnable(Runnable):
         Do stuff here. Can return a string or raise an exception.
         The progress_callback is a function expecting 1 value: current progress
         """
-        raise Exception("unimplemented")
+        # raise Exception("unimplemented")
+        client = dataiku.api_client()
         
