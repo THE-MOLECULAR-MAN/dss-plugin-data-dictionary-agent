3ea218e1a55762dbc87ecea01b8ad8d0fa351833
demo <tim.honker@dataiku.com>
2025-10-31 18:36:20 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -262,7 +262,7 @@ class MyRunnable(Runnable):
         #self.__tagname_aigen           = self.config["tagname_aigen"]
         #self.__tagname_do_not_autofill = self.config["tagname_DoNotAutoFill"]
         arr = self.config["object_types_to_autofill"]
-        print(f"list of obj to autofill {str(arr)})
+        print(f"list of obj to autofill {str(arr)}")
 
 #        ['projects', 'flowzones', 'datasets']
 
