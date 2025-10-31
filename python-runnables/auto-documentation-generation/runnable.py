7c305b41d8d9b5f4efa3a6bf7504d9796f631810
demo <tim.honker@dataiku.com>
2025-10-31 17:51:09 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -271,9 +271,9 @@ class MyRunnable(Runnable):
         # multi-select
         # pf = self.config["project_filter"] # all_projects, project_tags, project_folder
 
-        if self.config[""] == "this_project_only":
+        if self.config["project_filter"] == "this_project_only":
             self.__projects_list = [self.project_key]
-        
+        elif 
         self.__projects_list = self.client.list_project_keys()
 
     @property
