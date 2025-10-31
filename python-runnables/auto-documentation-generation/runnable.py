6ad62d910b79355d295245b2f3c8bfbaa10b337e
demo <tim.honker@dataiku.com>
2025-10-31 17:38:37 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -259,9 +259,11 @@ class MyRunnable(Runnable):
         self.__project_purpose = self.config["project_purpose"]
         self.__project_length = self.config["project_length"]
         self.__save_description = self.config["save_description"]
-        
-        pf = self.config["project_filter"] # projects, 
 
+        # multi-select
+        pf = self.config["project_filter"] # all_projects, project_tags, project_folder
+
+        
         # self.__projects_list = self.config.get("projects_list", [])
 
         self.__client = dataiku.api_client()
