2f23fc1af4c17afe73d3247f396f49a18d9f477b
demo <tim.honker@dataiku.com>
2025-11-20 18:19:50 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -259,7 +259,8 @@ class MyRunnable(Runnable):
         self.__project_purpose         = self.config["project_purpose"]
 
         self.__project_length          = self.config["project_length"]
-        self.__flowzone_purpose         = self.config["flowzone_purpose"]
+        self.__flowzone_purpose        = self.config["flowzone_purpose"]
+        self.__flowzone_length         = self.config["flowzone_length"]
         self.__save_description        = self.config["save_description"]
         #self.__tagname_aigen           = self.config["tagname_aigen"]
         #self.__tagname_do_not_autofill = self.config["tagname_DoNotAutoFill"]
