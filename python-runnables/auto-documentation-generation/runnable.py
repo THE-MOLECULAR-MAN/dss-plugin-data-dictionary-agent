9a207a0328cabd1ad8aa3b778d6856b0ce34195c
demo <tim.honker@dataiku.com>
2025-11-21 02:48:03 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -26,6 +26,7 @@ class MyRunnable(Runnable):
         :param config: the dict of the configuration of the object
         :param plugin_config: contains the plugin settings
         """
+        print('MyRunnable Constructor start')
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
