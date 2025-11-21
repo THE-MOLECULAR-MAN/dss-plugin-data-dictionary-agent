30a8382e55d86fa7c05a1e7ac01f85f6bb15d8a9
demo <tim.honker@dataiku.com>
2025-11-21 15:10:52 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -83,9 +83,10 @@ class MyRunnable(Runnable):
         If the runnable will return some progress info, have this function return a tuple of
         (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
         """
-        #return (self.__inscope, None)
+
         print('get_progress_target RAN')
-        return (100, None)
+        return (self.__inscope, None)
+        #return (100, None)
     
     def run_datasets(self, progress_callback):
         """
