07ac241691c2d0fb9f440ddb7bb10c69850b4572
demo <tim.honker@dataiku.com>
2025-11-21 15:07:06 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -153,7 +153,10 @@ class MyRunnable(Runnable):
                                 language=self.__language,
                                 save_description=self.__save_description,
                             )
-                            status = 'filled'
+                            if self.__save_description:
+                                status = 'Generated & updated'
+                            else:
+                                status = 'Generated but dry run, not updated'
 
                         #                 if SAVE_DESCRIPTION and dataset_has_full_documentation(project_handle, dataset_id):
                         #                     print(f"Successfully filled out all fields for {dataset_id}")
