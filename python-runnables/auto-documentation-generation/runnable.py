f022efac578764728e7d1c8d598362cb7a15a91c
admin <tim.honker@dataiku.com>
2026-01-08 15:12:17 +0000

    Edited file 'python-runnables/auto-documentation-generation/runnable.py'

--- python-runnables/auto-documentation-generation/runnable.py
+++ python-runnables/auto-documentation-generation/runnable.py
@@ -42,7 +42,7 @@ class MyRunnable(Runnable):
         self.__flowzone_length         = self.config["flowzone_length"]
         self.__save_description        = self.config["save_description"]
         # self.__overwrite_existing      = self.config["overwrite_existing"]
-        #self.__tagname_aigen           = self.config["tagname_aigen"]
+        self.__tagname           = self.config["tagname_aigen"]
         #self.__tagname_do_not_autofill = self.config["tagname_DoNotAutoFill"]
         #arr = self.config["object_types_to_autofill"]
         #print(f"list of obj to autofill {str(arr)}")
